0e5c0e005d4b5382399ac4acf2e675830ff599a6
Cathereum <106606021+Cathereum@users.noreply.github.com>
2023-04-20 14:19:33 +0700

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { IOption, IShippingFields } from "./app.interfaces";
+import Select from "react-select";
+import { useState } from "react";
+
+const options: IOption[] = [
+  { value: "russia", label: "Russia" },
+  { value: "usa", label: "USA" },
+  { value: "china", label: "China" },
+  { value: "paris", label: "Paris" },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    control,
+  } = useForm<IShippingFields>({ mode: "onChange" });
 
+  console.log("render");
+
+  const onSubmit: SubmitHandler<IShippingFields> = (data) => {
+    alert(`Your name ${data.adress.country}`);
+    reset();
+    console.log(data);
+  };
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register("name", { required: "Имя - обязательное поле!" })}
+          type="text"
+          placeholder="Введите Имя"
+        />
+        {errors.name && <p style={{ color: "red" }}>{errors.name.message}</p>}
+        <br />
+        <input
+          {...register("email", {
+            required: "Почта - обязательное поле!",
+            pattern: {
+              value:
+                /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/,
+              message: "Введите валидный email",
+            },
+          })}
+          type="text"
+          placeholder="Введите почту"
+        />
+        {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}
+
+        <Controller
+          control={control}
+          name="adress.country"
+          rules={{ required: "Выберите страну из списка!" }}
+          render={({ field: { onChange, value }, fieldState: { error } }) => (
+            <div>
+              <Select
+                placeholder={"Countries"}
+                options={options}
+                onChange={(optionValue) =>
+                  onChange((optionValue as IOption).value)
+                }
+              />
+              {error && <p style={{ color: "red" }}>{error.message}</p>}
+            </div>
+          )}
+        />
+
+        <div>
+          <button>send</button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
